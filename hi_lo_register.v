1832ab90b750240e3797ce479745f22b40d89485
saeed-abb <abbasi.sa77@gmail.com>
2018-11-16 10:28:23 +0330

    added hi_lo_register implementation

--- hi_lo_register.v
+++ hi_lo_register.v
@@ -4,8 +4,11 @@ module hi_lo_register(clk, rst, multLoad, hi_lo_in,
 	input wire clk, rst;
 	input wire multLoad;
 	input wire[31:0] hi_lo_in;
-	output wire[31:0] hi_lo_out;
+	output reg[31:0] hi_lo_out;
 
-	
+	always @(posedge clk) begin
+		if (multLoad)
+			hi_lo_out = hi_lo_in;
+	end
 
 endmodule // hi_lo_register
