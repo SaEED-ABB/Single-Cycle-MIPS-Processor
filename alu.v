cf8dff7095375611dda41ea91b8f76e695da0c18
saeed-abb <abbasi.sa77@gmail.com>
2018-11-16 11:21:17 +0330

    added alu, alu_control implementation

--- alu.v
+++ alu.v
@@ -6,6 +6,13 @@ module alu(ALUOperation, a, b,
 	output wire zero;
 	output wire[31:0] ALUResult;
 	
-	
+	assign ALUResult = (ALUOperation == 3'b010) ? a + b :
+						(ALUOperation == 3'b110) ? a - b :
+						(ALUOperation == 3'b000) ? a & b :
+						(ALUOperation == 3'b001) ? a | b :
+						(ALUOperation == 3'b111) ? a - b :
+						32'bx;
+
+	assign zero = (ALUOperation == 3'b111 && ALUResult == 32'b0) ? 1'b1 : 1'b0;
 	
 endmodule // alu
--- alu_control.v
+++ alu_control.v
@@ -7,5 +7,14 @@ module alu_control(clk, rst, instFunc, ALUOp,
 	output wire[2:0] ALUOperation;
 
 
+	assign ALUOperation = (ALUOp == 2'b00) ? 3'b010 :  // add
+						(ALUOp[0] == 1'b1) ? 3'b110 :  // sub
+						(ALUOp[1] == 1'b1) ? (instFunc[3:0] == 4'b0000 ? 3'b010 :  // add
+											instFunc[3:0] == 4'b0010 ? 3'b110 :  // sub
+											instFunc[3:0] == 4'b0100 ? 3'b000 :  // and
+											instFunc[3:0] == 4'b0101 ? 3'b001 :  // or
+											instFunc[3:0] == 4'b1010 ? 3'b111 :  // slt
+											3'bxxx) :
+						3'bxxx;
 
 endmodule // alu_control
